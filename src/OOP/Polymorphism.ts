2c5ac7b5fbb00da0c1f786a6ef0fe55af7ed7384
Stanislav Pankrashin <stanislav.pankrashin@roamdigital.com>
2022-06-28 15:53:31 +1200

    added some more comments

--- src/OOP/Polymorphism.ts
+++ src/OOP/Polymorphism.ts
@@ -3,20 +3,25 @@
  * Furthermore, subclasses can define their own functionality and override methods and members in a superclass, provided the method signatures match
  * 
  * This can be really useful in scenarios where we prescribe a common interface for a service that may have multiple implementations. E.g sending a txt message, sending an email, saving a file
+ * Polymorphism pairs really well with dependency injection in order to swap out services in modules as needed
  */
 
+// a baseline logger that was first implemented at the start of the project
 class ConsoleLogger {
     public logToConsole(message: string): void {
         console.log(message);
     }
 }
 
+// a new way we found to log to the console thats even better a few months down the line of the project being in PROD
 class BetterConsoleLogger extends ConsoleLogger {
     public override logToConsole(message: string): void {
         console.log(`BETTER_LOGGER_METHOD ${message}`);
     }
 }
 
+// a service used to consume a logger and log to the console
+// this also takes advantage of abstraction to not pay attention to the implementation of a consolelogger and instead just call the shared interface
 class LoggerConsumer {
     
     constructor(
@@ -28,6 +33,7 @@ class LoggerConsumer {
     }
 }
 
+// create a logger and consumer using the original method
 let logger: ConsoleLogger = new ConsoleLogger();
 let loggerConsumer: LoggerConsumer = new LoggerConsumer(logger);
 
